5d7533d5b7daa3ee160446dfc6000c82d151fc6f
kimdaae328 <kimdaae328@gmail.com>
2024-12-15 17:27:09 +0900

    10 pagination & filter 내림차순으로 정렬 완료

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
         <h2>게시글 목록</h2>
-        <hr class="my-4">
+        <hr class="my-4" />
         <div class="row g-3">
             <div v-for="post in posts" :key="post.id" class="col-4">
                 <PostItem :title="post.title" :content="post.content" :created-at="post.createdAt" @click="goPage(post.id)"></PostItem>
             </div>
         </div>
-        <hr class="my-4">
+        <hr class="my-4" />
         <AppCard>
             <PostDetailView :id="1"></PostDetailView>
         </AppCard>
@@ -15,41 +15,39 @@
 </template>
 
 <script setup>
-import PostItem from '@/components/posts/PostItem.vue';
-import PostDetailView from '@/views/posts/PostDetailView.vue'
-import AppCard from '../../components/AppCard.vue';
-import { getPosts } from '@/api/posts';
-import { ref } from 'vue';
-import { useRouter } from 'vue-router'
+    import PostItem from '@/components/posts/PostItem.vue';
+    import PostDetailView from '@/views/posts/PostDetailView.vue';
+    import AppCard from '../../components/AppCard.vue';
+    import { getPosts } from '@/api/posts';
+    import { ref } from 'vue';
+    import { useRouter } from 'vue-router';
 
-const router = useRouter();
-const posts = ref([]);
-const fetchPosts = async () => {
-    try {
-        const {data} = await getPosts();
-        posts.value = data;
-    } catch (error){
-        console.error(error)
-    }
-    // getPosts().then(response => {
-    //     console.log('response:', response);
-    // }).catch(error => {
-    //     console.log('error:', error);
-    // })
-}
-fetchPosts();
-const goPage = id => {
-    // router.push(`/posts/${id}`);
-    router.push({
-        name: 'PostDetail',
-        params: {
-            id,
+    const router = useRouter();
+    const posts = ref([]);
+    const params = ref({
+        _sort: 'createdAt',
+        _order: 'desc',
+    });
+    const fetchPosts = async () => {
+        try {
+            console.log('요청 파라미터:', params.value);
+            const { data } = await getPosts(params.value);
+            console.log('받은 데이터:', data);
+            posts.value = data;
+        } catch (error) {
+            console.error(error);
         }
-    })
-}
-
+    };
+    fetchPosts();
+    const goPage = id => {
+        // router.push(`/posts/${id}`);
+        router.push({
+            name: 'PostDetail',
+            params: {
+                id,
+            },
+        });
+    };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
