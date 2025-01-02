a15020e0fbabd5313a7000e22ca3d68a178497ff
kimdaae328 <kimdaae328@gmail.com>
2025-01-02 14:02:29 +0900

    12 공통 컴포넌트 분리

--- src/components/posts/PostFilter.vue
+++ src/components/posts/PostFilter.vue
@@ -2,7 +2,7 @@
     <form @submit.prevent>
         <div class="row g-3">
             <div class="col">
-                <input :value="title" @input="$emit('update:title', $event.target.value)"  type="text" class="form-control" />
+                <input :value="title" @input="$emit('update:title', $event.target.value)"  type="text" class="form-control" placeholder="제목으로 검색해주세요." />
             </div>
             <div class="col-3">
                 <select :value="limit" @input="$emit('update:limit', $event.target.value)" class="form-select">
