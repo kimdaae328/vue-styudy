eb4579adc7ca5fab038303386b69e3a624c21543
kimdaae328 <kimdaae328@gmail.com>
2025-01-03 20:59:36 +0900

    17 Teleport 컴포넌트 5:00

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -1,30 +1,32 @@
 <template>
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Launch demo modal
-    </button>
-    <div class="modal fade" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-backdrop fade show"></div>
+    <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Launch demo modal</button> -->
+    <div class="modal fade show d-block" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                ...
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">{{ title }}</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <slot></slot>
+                </div>
+                <div class="modal-footer">
+                    <slot name="actions">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </slot>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-
+    defineProps({
+        show: Boolean,
+        title: String,
+    });
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,46 @@
 <template>
     <div>About View</div>
-    <p>{{ $route.path }}</p>
-    <p>{{ $route.name }}</p>
-    <button class="btn btn-primary" @click="$router.push('/')">Home으로 이동</button>
+    <div>
+        <!-- 'button' 슬롯을 포함하여 Test 컴포넌트 사용 -->
+        <Test :title="test.title">
+            <template #button>
+                <span>내용 1</span>
+            </template>
+        </Test>
+
+        <!-- 'button' 슬롯 없이 Test 컴포넌트 사용 -->
+        <Test title="제목 2" />
+        <Test :title="title1.title" />
+
+        <div v-for="post in posts" key="post.id">
+            <Test :title2="post.title" :content2="post.content"></Test>
+        </div>
+    </div>
 </template>
 
-<script setup>
-    import { useRoute } from 'vue-router';
+<script>
+    import { reactive, ref } from 'vue';
+    import Test from '../components/Test.vue';
 
-    const routs = useRoute();
-    console.log('route.path', routs.path);
+    export default {
+        components: {
+            Test,
+        },
+        setup() {
+            const title1 = ref({ title: '동적인 제목1' });
+            const test = reactive({
+                title: '제목222222',
+            });
+            const posts = reactive([
+                { id: 1, title: '제목1', content: '내용1' },
+                { id: 2, title: '제목2', content: '내용2' },
+                { id: 3, title: '제목3', content: '내용3' },
+                { id: 4, title: '제목4', content: '내용4' },
+                { id: 5, title: '제목5', content: '내용5' },
+            ]);
+            return { test, title1, posts };
+        },
+    };
 </script>
 
 <style lang="scss" scoped></style>
