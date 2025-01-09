f57730865eeacdf7aaca7c88fa49f02d19d2c6a7
kimdaae328 <kimdaae328@gmail.com>
2025-01-09 13:40:50 +0900

    18 vue3 플러그인 만들기 완료

--- src/main.js
+++ src/main.js
@@ -3,12 +3,21 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import funcPlugins from './plugins/func';
+import objPlugins from './plugins/obj';
+import person from './plugins/person';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(funcPlugins);
+app.use(objPlugins, { name: '짐코딩' });
+app.use(person, {name: '홍길동'});
+app.use(router);
+app.mount('#app');
 import 'bootstrap/dist/js/bootstrap.js';
+// createApp(App).use(router).mount('#app');
 
-console.log('MODE:', import.meta.env.MODE);
-console.log('BASE_URL:', import.meta.env.BASE_URL);
-console.log('PROD:', import.meta.env.PROD);
-console.log('DEV:', import.meta.env.DEV);
-console.log('VITE_APP_API_URL:', import.meta.env.VITE_APP_API_URL);
+// console.log('MODE:', import.meta.env.MODE);
+// console.log('BASE_URL:', import.meta.env.BASE_URL);
+// console.log('PROD:', import.meta.env.PROD);
+// console.log('DEV:', import.meta.env.DEV);
+// console.log('VITE_APP_API_URL:', import.meta.env.VITE_APP_API_URL);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,21 @@
     <AppGrid :items="items" v-slot="{item}" colClass="col-6">
         <AppCard>{{item}}</AppCard>
     </AppGrid>
+    <hr class="my-4" />
+    <h2>{{ $person.name }}</h2>
+    <button class="btn btn-primary" @click="person.say">click person</button>
 </template>
 
+<script>
+export default {
+    created(){
+        // console.log(this.$person.name);
+        // this.$person.say();
+    },
+}
+</script>
 <script setup>
-import { ref } from 'vue';
+import { inject, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import AppCard from '@/components/AppCard.vue';
 import AppGrid from '@/components/AppGrid.vue';
@@ -20,7 +31,10 @@ const goAboutPage = () => {
     router.push('/about');
 };
 
-const items = ref(['사과','딸기','포도','바나나'])
+const items = ref(['사과','딸기','포도','바나나']);
+
+const person = inject('person');
+console.log('person.name: ', person.name)
 </script>
 
 <style lang="scss" scoped></style>
